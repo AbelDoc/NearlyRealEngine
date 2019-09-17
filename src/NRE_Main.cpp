87d7e8c8099a7af2d61e03d050a2b04a3b507f69
Louis Abel <louis.abel5@etu.univ-lorraine.fr>
2019-09-17 10:10:43 +0200

    Add simultaneous resolution rendering

--- src/NRE_Main.cpp
+++ src/NRE_Main.cpp
@@ -24,16 +24,15 @@
 
     class DevApplication : public Application {
         private :   // Field
-            VAO vao;
-            VBO<PrimitiveVertex> vbo;
+            VAO* vaos;
+            VBO<PrimitiveVertex>* vbos;
             PerspectiveCamera camera;
 
             bool wireframeMode;
-            float resolution;
 
         public :    // Methods
             //## Constructor ##//
-                DevApplication() : Application("NRE-System Devlopment", {1280, 720}, WindowStyle::RESIZEABLE, {8, 8, 8, 0, 0, 1, 24, 8, 0, 0, 0, 1, 2, 1}), vbo(GL_STATIC_DRAW), camera(50.0f, 70.0f, 1280.0f / 720.0f, Vector2D<float>(0.1f, 3000.0f), Vector3D<float>(8, 8, 8), Vector3D<float>(0, 1, 0)), wireframeMode(false), resolution(4) {
+                DevApplication() : Application("NRE-System Devlopment", {1280, 720}, WindowStyle::RESIZEABLE, {8, 8, 8, 0, 0, 1, 24, 8, 0, 0, 0, 1, 2, 1}), vaos(new VAO[4]), vbos(new VBO<PrimitiveVertex>[4]{GL_STATIC_DRAW, GL_STATIC_DRAW, GL_STATIC_DRAW, GL_STATIC_DRAW}), camera(50.0f, 70.0f, 1280.0f / 720.0f, Vector2D<float>(0.1f, 3000.0f), Vector3D<float>(8, 8, 8), Vector3D<float>(0, 1, 0)), wireframeMode(false) {
                     updateChunks();
 
                     glEnable(GL_DEPTH_TEST);
@@ -65,14 +64,6 @@
                                 polygonMode(GL_FRONT, GL_FILL);
                             }
                             return true;
-                        } else if (event.isCode(KeyCode::R)) {
-                            resolution *= 2;
-                            updateChunks();
-                            return true;
-                        } else if (event.isCode(KeyCode::E)) {
-                            resolution /= 2;
-                            updateChunks();
-                            return true;
                         }
                         return false;
                     });
@@ -90,28 +81,51 @@
                     Primitive3D* shader = ProgramManager::get<Primitive3D>();
                     shader->bind();
                         shader->sendMVP(static_cast <PerspectiveCamera const&> (camera).getProjection() * camera.getView());
-                        vao.bind();
-                            vbo.draw();
-                        vao.unbind();
+                        std::size_t index = 0;
+                        unsigned int w, h;
+                        w = 1280 / 2;
+                        h = 720 / 2;
+                        for (int i = 0; i < 2; i++) {
+                            for (int j = 0; j < 2; j++) {
+                                VBO<PrimitiveVertex>& vbo = vbos[index];
+                                VAO& vao = vaos[index++];
+                                glViewport(i * w, j * h, w, h);
+                                vao.bind();
+                                    vbo.draw();
+                                vao.unbind();
+                            }
+                        }
                     shader->unbind();
                 }
                 void destroy() override {
+                    delete[] vbos;
+                    delete[] vaos;
                 }
                 void updateChunks() {
-                    for (int z = -5; z <= 5; z++) {
-                        for (int x = -5; x <= 5; x++) {
-                            Chunk chunk({static_cast <int> (Chunk::SIZE_X) * x, 0, static_cast <int> (Chunk::SIZE_Z) * z});
-                            ChunkFactory::createTerrain(chunk, resolution);
-                            ChunkPolygonizer::polygonize(chunk, vbo);
+                    std::size_t index = 0;
+                    for (int i = 0; i < 2; i++) {
+                        for (int j = 0; j < 2; j++) {
+                            VBO<PrimitiveVertex>& vbo = vbos[index];
+                            VAO& vao = vaos[index];
+
+                            float resolution = static_cast <float> (std::pow(2.0f, index++)) / 2.0f;
+
+                            for (int z = -5; z <= 5; z++) {
+                                for (int x = -5; x <= 5; x++) {
+                                    Chunk chunk({static_cast <int> (Chunk::SIZE_X) * x, 0, static_cast <int> (Chunk::SIZE_Z) * z});
+                                    ChunkFactory::createTerrain(chunk, resolution);
+                                    ChunkPolygonizer::polygonize(chunk, vbo);
+                                }
+                            }
+
+                            std::cout << "Chunks update :" << std::endl;
+                            std::cout << "\tResolution : " << resolution << std::endl;
+                            std::cout << "\tVertex count : " << vbo.getDataCount() << std::endl;
+
+                            vbo.allocateAndFill();
+                            vao.access(&vbo);
                         }
                     }
-
-                    std::cout << "Chunks update :" << std::endl;
-                    std::cout << "\tResolution : " << resolution << std::endl;
-                    std::cout << "\tVertex count : " << vbo.getDataCount() << std::endl;
-
-                    vbo.allocateAndFill();
-                    vao.access(&vbo);
                 }
     };
 
